e27959c1aae7340db084e1e94a99300108f250bb
brodyl <75949899+brodyl@users.noreply.github.com>
2024-11-21 17:37:36 -0700

    Update tutorial_11-2_ROT13_Encyption.py
    Small change to include a check for successful read()

--- tutorial_11-2_ROT13_Encyption.py
+++ tutorial_11-2_ROT13_Encyption.py
@@ -1,17 +1,6 @@
-
-'''
-Example python script which will 
-(1) Open a text file
-(2) Encrypt the contents using the ROT13 Cipher
-(3) Overwrite the contents of teh file with the encrypted string
-
-This is only one way you could solve this problem. Whichever way you solve 
-the problem, ensure you always use try/except blocks when opening files on 
-the computer
-'''
-
-filename = "paragraph.txt"
+filename = "paragrdaph.txt"
 new_string = ""                         # Empty string to hold the new encrypted text
+read_succesful = False                  # Flag to check if the file was read succesfully
 
 try:
     with open(filename, "r") as fileHandler:
@@ -34,20 +23,24 @@ try:
     # But if using other methods of opening the file you would have to explicitly close the file like so:
     # fileHandler.close()
 
+    read_succesful = True               # If we reach this line it means we've successfully encrypted the file
+                                        # But we have not overwritten the file yet
+
 except:
     print(f"Error attempting to read the file {filename}")
 
 
 # Try to write the new encrypted string to the same file
-try:
-    # Overwrite the text file with the new string
-    # I will demonstrate a different method to open/close a file if not using the [with ... as ...] method
-    fileHandler = open(filename, "w")
-    fileHandler.write(new_string)       # Overwrite with the modified content
-    fileHandler.close()                 # Always ensure a file is closed when finished
+# but only if we've successully encrypted it in the previous step
+if (read_succesful):
+    try:
+        # Overwrite the text file with the new string
+        # I will demonstrate a different method to open/close a file if not using the [with ... as ...] method
+        fileHandler = open(filename, "w")
+        fileHandler.write(new_string)       # Overwrite with the modified content
+        fileHandler.close()                 # Always ensure a file is closed when finished
 
-    print("Done!")
+        print("Done!")
 
-except:
-    print(f"Error attempting to write to file {filename}")
-
+    except:
+        print(f"Error attempting to write to file {filename}")
