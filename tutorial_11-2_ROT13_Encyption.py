7149314ea56cfd9af1184c657e4330dc6a7952c8
brodyl <75949899+brodyl@users.noreply.github.com>
2024-11-21 17:31:30 -0700

    Create tutorial_11-2_ROT13_Encyption.py

---
+++ tutorial_11-2_ROT13_Encyption.py
@@ -0,0 +1,53 @@
+
+'''
+Example python script which will 
+(1) Open a text file
+(2) Encrypt the contents using the ROT13 Cipher
+(3) Overwrite the contents of teh file with the encrypted string
+
+This is only one way you could solve this problem. Whichever way you solve 
+the problem, ensure you always use try/except blocks when opening files on 
+the computer
+'''
+
+filename = "paragraph.txt"
+new_string = ""                         # Empty string to hold the new encrypted text
+
+try:
+    with open(filename, "r") as fileHandler:
+
+        char = fileHandler.read(1)
+        while char:
+            if "a" <= char <= "m" or "A" <= char <= "M":
+                num = ord(char) + 13    # Add 13 to the ASCII code of char if it's from [a-m] or [A-M]
+                new_string += chr(num)  # Add the new character to the new string
+
+            elif "n" <= char <= "z" or "N" <= char <= "Z":
+                num = ord(char) - 13    # Subtract 13 from the ASCII code of char if it's from [n-z] or [N-Z]
+                new_string += chr(num)  # Add the new character to the new string
+
+            else:
+                new_string += char      # Add the original character to the new string
+            char = fileHandler.read(1)
+
+    # [with open("paragraph.txt", "r") as fileHandler] automatically closes the fileHandler when completed
+    # But if using other methods of opening the file you would have to explicitly close the file like so:
+    # fileHandler.close()
+
+except:
+    print(f"Error attempting to read the file {filename}")
+
+
+# Try to write the new encrypted string to the same file
+try:
+    # Overwrite the text file with the new string
+    # I will demonstrate a different method to open/close a file if not using the [with ... as ...] method
+    fileHandler = open(filename, "w")
+    fileHandler.write(new_string)       # Overwrite with the modified content
+    fileHandler.close()                 # Always ensure a file is closed when finished
+
+    print("Done!")
+
+except:
+    print(f"Error attempting to write to file {filename}")
+
